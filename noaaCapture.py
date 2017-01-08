8bb1d73eebf3aa88b10b47c0cb9d63c42587660a
Dion <Dion@Dions-MacBook-Pro.local>
2017-01-08 13:45:16 -0800

    first working versioon

--- noaaCapture.py
+++ noaaCapture.py
@@ -1,7 +1,8 @@
-# return a log file of when a web file is updated
+ log file of when a web file is updated
 # option to save down updated files - to collect all unique files in a directory
 # option to save down time file checked and boolian if updated - for analyzing the pattern of update times
 # not sure how to keep the last file details without reading from disk when using cron, so using sleep
+# todo: date format from file reading, logfile
 
 import time, datetime
 import os, urllib2, urllib
@@ -10,9 +11,6 @@ import os, urllib2, urllib
 # format = "%Y%M%D-%H:%M:%S"
 # print str(curTime)
 
-
-
-
 os.chdir('/Users/dion/Dropbox/_FUN/Coding/dionProjects/WebCapture/')
 
 saveFile = True
@@ -20,28 +18,43 @@ targetUrl = 'http://www.opc.ncep.noaa.gov/P_sfc_full_ocean_color.png'
 prevFileTime = ''
 currFileTime = ''
 logFileName = 'noaaCaptureLog-'+ str(datetime.datetime.now()) + '.csv'
+print 'logFileName = ' + logFileName # does print need ()?
 logFile = open(logFileName, 'w')
 
-for i in range(5):   # while(true)
+for i in range(1000000):   # while(true)
 
     targetFile = urllib2.urlopen(targetUrl)
-    currFileTime = targetFile.info().getdate('last-modified')
+    currFileTime = targetFile.info().getdate('Last-Modified')
     # print currFileTime, prevFileTime
-    logFile.write(str(datetime.datetime.now())+','+ str(currFileTime)+'\n')
-    print (str(datetime.datetime.now()), str(currFileTime))
+    logFile.write(str(i) + ' ' + str(datetime.datetime.now())+','+ str(currFileTime)+'\n')
+    print (str(i) +' ' +str(datetime.datetime.now()), str(currFileTime))
+    # print
+    # print 'CURRENT FILE TIME'
+    # print datetime(currFileTime).ctime('')
+    # print datetime.datetime(int(currFileTime))
+    # print time(currFileTime)
+    # print
+
 
     if (currFileTime != prevFileTime):
         #append time and currFileTime to log file
-        logFile.write('new file\n')
+        logFile.write('new file since '+ 'x' + '\n') # time diff here
+        print ('new file since '+ 'x' + '\n') # time diff here
+        print (str(currFileTime) + str(prevFileTime))
+        logFile.write(str(currFileTime) + str(prevFileTime))
 
         if saveFile:
+            print '\n'
+            print targetFile.info()
             saveFileName = 'png/noaa-'+ str(currFileTime) +'.png'   #yymmdd:hm:mm:ss"
             # print str(currFileTime)
             print saveFileName
+            print
             urllib.urlretrieve(targetUrl, saveFileName)
 
     prevFileTime = currFileTime
     targetFile.close()
-    time.sleep(1)
+    time.sleep(30)
 
 logFile.close()
+
